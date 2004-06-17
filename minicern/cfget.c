ee24149241bfdab884b3dc752bea5e91edffd347
Fons Rademakers <Fons.Rademakers@cern.ch>
2004-06-17 13:57:16 +0000

    remove unused label.

--- minicern/cfget.c
+++ minicern/cfget.c
@@ -1,7 +1,12 @@
 /*
- * $Id: cfget.c,v 1.1.1.1 2002/07/24 15:56:28 rdm Exp $
+ * $Id: cfget.c,v 1.2 2002/12/02 16:37:45 brun Exp $
  *
  * $Log: cfget.c,v $
+ * Revision 1.2  2002/12/02 16:37:45  brun
+ * Changes from Federico Carminati and Peter Hristov who ported the system
+ * on the Ithanium processors.It is tested on HP, Sun, and Alpha, everything
+ * seems to work. The optimisation is switched off in case of gcc2.xx.yyy
+ *
  * Revision 1.1.1.1  2002/07/24 15:56:28  rdm
  * initial import into CVS
  *
@@ -89,7 +94,7 @@ void type_of_call CFGET(lundes, medium, nwrec, nwtak, mbuf,
       nbdn   = read (fildes, mbuf, nbdo);
       if (nbdn == 0)               goto heof;
       if (nbdn <  0)               goto herror;
-retn: *nwtak = (nbdn - 1) / NBYTPW + 1;
+      *nwtak = (nbdn - 1) / NBYTPW + 1;
       return;
 
 /*        Handle exceptions        */
