0feb951e5504e2677e4d9919d50ed4497f3fcabf
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2023-02-09 14:17:20 +0100

    Making version 4.2

--- TGeant3/TG3Version.h
+++ TGeant3/TG3Version.h
@@ -6,7 +6,7 @@
 ///
 /// \author I. Hrivnacova; IJCLab, Orsay
 
-#define GEANT3_VMC_RELEASE "4.1"
-#define GEANT3_VMC_RELEASE_DATE "17 March 2022"
+#define GEANT3_VMC_RELEASE "4.2"
+#define GEANT3_VMC_RELEASE_DATE "9 February 2023"
 
 #endif // ROOT_TG3VERSION
